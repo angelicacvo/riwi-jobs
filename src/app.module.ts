fbb65846bf52992843925d5d0ffebe9336f8ce72
Angelica Cuervo <108093638+angelicacvo@users.noreply.github.com>
2026-01-05 06:45:00 -0500

    feat: modular infrastructure folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { VacanciesModule } from './vacancies/vacancies.module';
+import { ApplicationsModule } from './applications/applications.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, VacanciesModule, ApplicationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
