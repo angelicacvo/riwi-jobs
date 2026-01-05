09a1af7f37bd7eed8f81d1c1490289d60a878cab
Angelica Cuervo <108093638+angelicacvo@users.noreply.github.com>
2026-01-05 09:19:20 -0500

    feat: vacancies CRUD

--- src/vacancies/vacancies.service.ts
+++ src/vacancies/vacancies.service.ts
@@ -1,26 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateVacancyDto } from './dto/create-vacancy.dto';
 import { UpdateVacancyDto } from './dto/update-vacancy.dto';
+import { Vacancy } from './entities/vacancy.entity';
 
 @Injectable()
 export class VacanciesService {
-  create(createVacancyDto: CreateVacancyDto) {
-    return 'This action adds a new vacancy';
+  constructor(
+    @InjectRepository(Vacancy)
+    private vacanciesRepository: Repository<Vacancy>,
+  ) {}
+
+  async create(createVacancyDto: CreateVacancyDto): Promise<Vacancy> {
+    const vacancy = this.vacanciesRepository.create(createVacancyDto);
+    return await this.vacanciesRepository.save(vacancy);
   }
 
-  findAll() {
-    return `This action returns all vacancies`;
+  async findAll(): Promise<Vacancy[]> {
+    return await this.vacanciesRepository.find({
+      order: { createdAt: 'DESC' },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} vacancy`;
+  async findOne(id: string): Promise<Vacancy> {
+    const vacancy = await this.vacanciesRepository.findOne({
+      where: { id },
+    });
+
+    if (!vacancy) {
+      throw new NotFoundException(`Vacancy with ID ${id} not found`);
+    }
+
+    return vacancy;
   }
 
-  update(id: number, updateVacancyDto: UpdateVacancyDto) {
-    return `This action updates a #${id} vacancy`;
+  async update(id: string, updateVacancyDto: UpdateVacancyDto): Promise<Vacancy> {
+    const vacancy = await this.findOne(id);
+    Object.assign(vacancy, updateVacancyDto);
+    return await this.vacanciesRepository.save(vacancy);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} vacancy`;
+  async remove(id: string): Promise<void> {
+    const vacancy = await this.findOne(id);
+    await this.vacanciesRepository.remove(vacancy);
   }
 }
