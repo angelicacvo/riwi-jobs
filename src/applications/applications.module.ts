ee3951f8afbbb8ce1160510e455279474e758f57
Angelica Cuervo <108093638+angelicacvo@users.noreply.github.com>
2026-01-05 09:41:24 -0500

    feat: applications CRUD and validations

--- src/applications/applications.module.ts
+++ src/applications/applications.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApplicationsService } from './applications.service';
 import { ApplicationsController } from './applications.controller';
+import { Application } from './entities/application.entity';
+import { VacanciesModule } from '../vacancies/vacancies.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Application]),
+    VacanciesModule,
+  ],
   controllers: [ApplicationsController],
   providers: [ApplicationsService],
 })
